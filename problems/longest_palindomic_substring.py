97a64d33abea8a788b53af00eb957e6333f1d15b
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-07-04 16:41:58 -0600

    Removed unnecessary variables in place of a substring range

--- problems/longest_palindomic_substring.py
+++ problems/longest_palindomic_substring.py
@@ -3,14 +3,12 @@ from collections import deque
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest_substring: str = ''
-        curr_chars: str
         curr_substring: str
         center: str
 
         i: int; l: int; r: int
         char: str
         for i, char in enumerate(s):
-            curr_chars = ''
             r = 1
 
             while True:
@@ -19,7 +17,6 @@ class Solution:
 
                 r += 1
 
-            center = r * char
             l = i - 1 # l is now at the left of center start
             r += i # r is now at the right of center end
 
@@ -27,21 +24,10 @@ class Solution:
                 if l < 0 or r >= len(s) or s[l] != s[r]:
                     break
 
-                curr_chars += s[r]
                 l -= 1
                 r += 1
 
-            if len(center) + 2 * len(curr_chars) > len(longest_substring):
-                longest_substring = self.palindrome_string(center, curr_chars)
+            if len(s[l+1:r]) > len(longest_substring):
+                longest_substring = s[l+1:r]
 
         return longest_substring
-
-
-    @staticmethod
-    def palindrome_string(center: str, substring: str = None) -> str:
-        if not substring:
-            return center
-        elif len(substring) == 1:
-            return substring + center + substring
-
-        return substring[::-1] + center + substring
